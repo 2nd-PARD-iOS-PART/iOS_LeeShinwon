d5d26a78dc8dd7e60d071109ca32658a98980980
LeeShinwon <leeshin1130@gmail.com>
2023-09-30 15:14:09 +0900

    HW3 tabBar

---
+++ HW3/HW3/Controller/DownloadsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  DownloadsViewController.swift
+//  HW3
+//
+//  Created by 이신원 on 2023/09/30.
+//
+
+import UIKit
+
+class DownloadsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground
+    }
+    
+
+}
---
+++ HW3/HW3/Controller/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  HomeViewController.swift
+//  HW3
+//
+//  Created by 이신원 on 2023/09/30.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground
+    }
+
+
+}
