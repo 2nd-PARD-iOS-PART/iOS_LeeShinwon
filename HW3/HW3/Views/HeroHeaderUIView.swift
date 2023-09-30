3755df8ba16cd8f319d0a20aaf924590d91d10eb
LeeShinwon <leeshin1130@gmail.com>
2023-09-30 20:45:44 +0900

    frame label

--- HW3/HW3/Views/HeroHeaderUIView.swift
+++ HW3/HW3/Views/HeroHeaderUIView.swift
@@ -8,6 +8,25 @@
 import UIKit
 
 class HeroHeaderUIView: UIView {
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 2
+        label.text = "Stranger \nThings"
+        label.textAlignment = .center
+        label.font = UIFont.systemFont(ofSize: 35, weight: .heavy)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let subTitleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "TV Shows • textTwo • TV Shows • US"
+        label.textAlignment = .center
+        label.font = UIFont.systemFont(ofSize: 12, weight: .medium)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
     private let heroImageView: UIImageView = {
         let imageView = UIImageView()
         imageView.contentMode = .scaleToFill
@@ -16,9 +35,38 @@ class HeroHeaderUIView: UIView {
         return imageView
     }()
     
+    private func addGradient(){
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [
+            UIColor.clear.cgColor,
+            UIColor.systemBackground.cgColor
+        ]
+        gradientLayer.frame = bounds
+        layer.addSublayer(gradientLayer)
+    }
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         addSubview(heroImageView)
+        addGradient()
+        addSubview(titleLabel)
+        addSubview(subTitleLabel)
+        applyConstraints()
+    }
+    
+    private func applyConstraints(){
+        let titleLabelConstraints = [
+            titleLabel.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+            titleLabel.topAnchor.constraint(equalTo: topAnchor, constant: 200)
+        ]
+        
+        let subTitleLabelConstraints = [
+            subTitleLabel.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+            subTitleLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 10)
+        ]
+        
+        NSLayoutConstraint.activate(titleLabelConstraints)
+        NSLayoutConstraint.activate(subTitleLabelConstraints)
     }
     
     override func layoutSubviews() {
